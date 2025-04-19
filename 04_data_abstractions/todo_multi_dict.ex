b96378bba716d456e6041bbe63ec20b72c84aeb1
ja-odur <odurjoseph8@gmail.com>
2025-04-19 11:39:59 +0300

    represent entries with maps

--- 04_data_abstractions/todo_multi_dict.ex
+++ 04_data_abstractions/todo_multi_dict.ex
@@ -13,8 +13,8 @@ end
 defmodule TodoList do
   def new(), do: MultiDict.new()
   
-  def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title)
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
   end
   
   def entries(todo_list, date) do
