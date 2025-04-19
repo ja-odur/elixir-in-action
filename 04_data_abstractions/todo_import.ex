e5deb075dea438ae298893db69ffe39c3200c7eb
ja-odur <odurjoseph8@gmail.com>
2025-04-20 01:02:11 +0300

    Add todo file import

--- 04_data_abstractions/todo_import.ex
+++ 04_data_abstractions/todo_import.ex
@@ -48,3 +48,29 @@ defmodule TodoList do
   end
   
 end
+
+defmodule TodoList.CsvImporter do
+  def import(file_name) do
+    file_name
+    |> read_lines
+    |> create_entries
+    |> TodoList.new()
+  end
+  
+  def read_lines(file_name) do
+    file_name
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing(&1, "\n"))
+  end
+  
+  def create_entries(lines) do
+    Stream.map(
+      lines,
+      fn line ->
+       [date_string, title] = String.split(line, ",")
+       date = Date.from_iso8601!(date_string)
+       %{date: date, title: title}
+      end
+    )
+  end
+end
